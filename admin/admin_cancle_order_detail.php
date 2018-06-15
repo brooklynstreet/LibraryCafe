4c826515ee3ccd639a30dcd12f9a62a970fcb346
brooklynstreet <brklynst@gmail.com>
2018-06-16 01:25:42 +0700

    เพิ่ม code

--- admin/admin_cancle_order_detail.php
+++ admin/admin_cancle_order_detail.php
@@ -1,5 +1,6 @@
 <?php
     require_once('../library_function.php');
+    $order_detail_id = $_GET['order_detail_id'];
     $newStatus = "cancle";
     change_orderDetail_status($order_detail_id, $newStatus);
 
